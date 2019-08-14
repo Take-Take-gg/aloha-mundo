1e4a023936b41f71e96501c73637fe3d602cb5ca
Alumno <Alumno@PC22>
2019-08-14 12:15:42 -0500

    hikaaaaaaaa

---
+++ ufff c shard/ufff c shard/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ufff_c_shard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a eleo mundo");
+            Console.WriteLine("-----"); Console.Write("jjsjs");
+        }
+    }
+}
